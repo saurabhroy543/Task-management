cd97b9afdfb7577ff48b28a6af382f291a127220
Shubham Saurabh <71812923+saurabhroy543@users.noreply.github.com>
2023-05-03 00:15:15 +0530

    Update settings.py

--- task_management/settings.py
+++ task_management/settings.py
@@ -105,8 +105,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILE_DIRS = (os.path.join(BASE_DIR, 'static'))
+import os
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
